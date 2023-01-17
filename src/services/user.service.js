b1f97098d865b71ede4be65ab2d9c0c67bf13b67
Anjay-2022 <anjay2013@gmail.com>
2023-01-17 10:27:42 +0530

    ADDED:BcryptHashPassword

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,21 +1,25 @@
 import User from '../models/user.model';
-
+import bcrypt from 'bcrypt';
 
 //Login users
 export const loginUser = async (body) => {
   const data = await User.findOne({email:body.email});
   if (data != null){
-   if(body.password==data.password)
+    await bcrypt.compare(body.password,data.password)
+    .then(function(result){
+      if(!result)
+      throw new error ("invalid credential")
+    } )
       return data
-    else
-     throw new Error("Invalid Credential")
  } else
       throw new Error("Login unsuccesfull")
 };
 
-
-//register new user
-export const registerUser = async (body) => {
+//create new user
+export const newUser = async (body) => {
+  const saltrounds= await bcrypt.genSalt(10);
+  const hashedpwd = await bcrypt.hash(body.password,saltrounds)
+  body.password=hashedpwd
   const data = await User.create(body);
   return data;
 };
